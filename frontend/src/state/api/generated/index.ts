720631fa84d688eb4b3fd75b58e6d5fcfcaa4372
nilsrothe <nils.rothe@healthinal.com>
2024-03-03 14:54:11 +0100

    feat(LERN-5): initial frontend

---
+++ frontend/src/state/api/generated/index.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelError, CancelablePromise } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { UserDto } from './models/UserDto';
+
+export { UserRessourceService } from './services/UserRessourceService';
---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3005,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
