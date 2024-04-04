d766058993d520433c1ac7d0377f6c3e1a63b20b
Ale <alejandro.wenzler@gmail.com>
2024-04-05 00:30:02 +0200

    feat(LERN-48): added/changed backoffice endpoints.

--- backend/src/main/kotlin/ch/learntrack/backend/backoffice/user/UserDto.kt
+++ backend/src/main/kotlin/ch/learntrack/backend/backoffice/user/UserDto.kt
@@ -1,9 +1,22 @@
 package ch.learntrack.backend.backoffice.user
 
+import java.time.LocalDateTime
 import java.util.UUID
 
 public data class UserDto(
     val id: UUID,
     val firstname: String,
     val lastname: String,
+    val email: String,
+    val birthdate: LocalDateTime?,
+    val created: LocalDateTime?,
+    val updated: LocalDateTime?,
+)
+
+public data class CreateUserDto(
+    val firstname: String,
+    val middlename: String?,
+    val lastname: String,
+    val email: String,
+    val password: String,
 )
