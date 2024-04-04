d819207e40e57a10399b7a145d961735eead7a3e
Quintexxx <martin.brunner06@gmail.com>
2024-04-04 19:29:06 +0200

    feat(LERN-12): added Functionality and Error Display

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -4,9 +4,9 @@ import LearnTrackLogo from '../assets/logo.svg?react';
 export function Logo() {
   return (
     <LogoContainer>
-      <LearnTrackLogo />
+      <LearnTrackLogo width={'80px'} />
       <Typography level="h2" component={'h2'} sx={{ color: `#E191A1`, marginLeft: '20px' }}>
-        <Typography sx={{ color: '#89DAD5' }}>Learn</Typography> UP
+        <Typography sx={{ color: '#89DAD5' }}>Learn</Typography> Track
       </Typography>
     </LogoContainer>
   );
