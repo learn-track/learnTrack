c279bb376e6216fb77e84cf11f0a60a9b1538914
A1eeee <alejandro.wenzler@gmail.com>
2024-04-06 10:17:46 +0200

    feat(LERN-43): added school endpoint, create school with validation check.

--- backend/src/main/kotlin/ch/learntrack/backend/backoffice/school/SchoolRessource.kt
+++ backend/src/main/kotlin/ch/learntrack/backend/backoffice/school/SchoolRessource.kt
@@ -1,6 +1,8 @@
 package ch.learntrack.backend.backoffice.school
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController
 public class SchoolRessource(private val schoolService: SchoolService) {
     @GetMapping
     public fun getAllSchools(): List<SchoolDto> = schoolService.getAllSchools()
+
+    @PostMapping("/create")
+    public fun createSchool(@RequestBody createSchoolDto: CreateSchoolDto) {
+        schoolService.createSchool(createSchoolDto)
+    }
 }
--- backend/src/main/kotlin/ch/learntrack/backend/backoffice/school/SchoolService.kt
+++ backend/src/main/kotlin/ch/learntrack/backend/backoffice/school/SchoolService.kt
@@ -1,9 +1,12 @@
 package ch.learntrack.backend.backoffice.school
 
 import ch.learntrack.backend.common.EntityService
+import ch.learntrack.backend.common.LearnTrackConflictException
 import ch.learntrack.backend.persistence.tables.daos.SchoolDao
 import ch.learntrack.backend.persistence.tables.pojos.School
 import ch.learntrack.backend.persistence.tables.records.SchoolRecord
+import java.time.LocalDateTime
+import java.util.UUID
 
 public class SchoolService(
     private val schoolDao: SchoolDao,
@@ -19,4 +22,28 @@ public class SchoolService(
     )
 
     public fun getAllSchools(): List<SchoolDto> = schoolDao.fetchAllSchools().map(::mapToDto)
+
+    public fun createSchool(createSchoolDto: CreateSchoolDto) {
+        if (isSchoolExisting(createSchoolDto)) {
+            throw LearnTrackConflictException("School already exists")
+        }
+
+        val school = School(
+            id = UUID.randomUUID(),
+            name = createSchoolDto.name.trim(),
+            address = createSchoolDto.address.trim(),
+            city = createSchoolDto.city.trim(),
+            postcode = createSchoolDto.postcode,
+            created = LocalDateTime.now(),
+            updated = LocalDateTime.now(),
+        )
+        schoolDao.insert(school)
+    }
+
+    public fun isSchoolExisting(createSchoolDto: CreateSchoolDto): Boolean = schoolDao.fetchAllSchools().any {
+        it.name == createSchoolDto.name.trim() &&
+                it.address == createSchoolDto.address.trim() &&
+                it.city == createSchoolDto.city.trim() &&
+                it.postcode == createSchoolDto.postcode
+    }
 }
