69882793f13a2c973bd6550fdc394c9986ac6ff8
A1eeee <alejandro.wenzler@gmail.com>
2024-05-23 20:12:07 +0200

    feat(LERN-78): added E-mail if registered check and tests

--- backend/src/main/kotlin/ch/learntrack/backend/user/UserRessource.kt
+++ backend/src/main/kotlin/ch/learntrack/backend/user/UserRessource.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -28,6 +29,9 @@ public class UserRessource(
             token = tokenService.createJwtToken(user.id),
         )
     }
+
+    @GetMapping("/register/check-email-free")
+    public fun isEmailFree(@RequestParam email: String): Boolean = userService.findUserByEmail(email) == null
 
     @PostMapping("/register")
     public fun register(@RequestBody createUserDto: CreateUserDto): LoginResponseDto {
