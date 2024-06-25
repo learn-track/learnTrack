b11fcb15699b47e5418eb5e934940a6ff7813372
SleepyPeel <shay.h.moser@gmail.com>
2024-06-25 22:21:33 +0200

    feat(LERN-49): Implemented getAllSchoolsForAdmin endpoint.

--- backend/src/main/kotlin/ch/learntrack/backend/backoffice/school/SchoolQueries.kt
+++ backend/src/main/kotlin/ch/learntrack/backend/backoffice/school/SchoolQueries.kt
@@ -3,9 +3,20 @@ package ch.learntrack.backend.backoffice.school
 import ch.learntrack.backend.persistence.tables.daos.SchoolDao
 import ch.learntrack.backend.persistence.tables.pojos.School
 import ch.learntrack.backend.persistence.tables.references.SCHOOL
+import ch.learntrack.backend.persistence.tables.references.USER_SCHOOL
+import java.util.UUID
 
 public fun SchoolDao.fetchAllSchools(): MutableList<School> = ctx()
     .select()
     .from(SCHOOL)
+    .fetch()
+    .into(School::class.java)
+
+public fun SchoolDao.fetchAllSchoolsForAdmin(userId: UUID): MutableList<School> = ctx()
+    .select(SCHOOL)
+    .from(USER_SCHOOL)
+    .join(SCHOOL)
+    .on(SCHOOL.ID.eq(USER_SCHOOL.SCHOOL_ID))
+    .and(USER_SCHOOL.USER_ID.eq(userId))
     .fetch()
     .into(School::class.java)
