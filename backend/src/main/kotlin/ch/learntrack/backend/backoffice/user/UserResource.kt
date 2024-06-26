efc801fb4ac4db1832cfb95c25a0a651d7585895
SleepyPeel <shay.h.moser@gmail.com>
2024-06-26 21:07:20 +0200

    feat(LERN-47): Implemented Teacher and Student Count endpoint
    feat(LERN-47): Implemented Teacher and Student count endpoint.

--- backend/src/main/kotlin/ch/learntrack/backend/backoffice/user/UserResource.kt
+++ backend/src/main/kotlin/ch/learntrack/backend/backoffice/user/UserResource.kt
@@ -1,11 +1,16 @@
 package ch.learntrack.backend.backoffice.user
 
 import ch.learntrack.backend.backoffice.BACKOFFICE_ROOT_URL
+import ch.learntrack.backend.persistence.UserRole
+
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
+import java.util.UUID
 
 @RestController
 @RequestMapping("$BACKOFFICE_ROOT_URL/user")
@@ -16,4 +21,17 @@ public class UserResource(private val userService: UserService) {
     @PostMapping("/create")
     public fun createAdminUser(@RequestBody createUserDto: CreateUserDto): Unit =
         userService.createAdminUser(createUserDto)
+
+    @GetMapping("/getAllAdminsBySchoolId")
+    public fun getAllAdminsForSchool(@RequestParam schoolId: UUID): List<UserDto> = userService.getAllAdminsBySchoolId(
+        schoolId,
+    )
+
+    @GetMapping("/getTeacherCount")
+    public fun getTeacherCounts(@RequestParam schoolId: UUID): Int = userService.getUserCount(schoolId,
+        UserRole.TEACHER)
+
+    @GetMapping("/getStudentCount")
+    public fun getStudentCounts(@RequestParam schoolId: UUID): Int = userService.getUserCount(schoolId,
+        UserRole.STUDENT)
 }
