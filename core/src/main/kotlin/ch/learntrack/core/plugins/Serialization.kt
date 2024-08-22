0d736f8e0268222fa3684bbfd6a8595ac1966570
nilsrothe <nils.rothe@healthinal.com>
2024-08-22 21:00:03 +0200

    feat(LERN-106): setup ktor

---
+++ core/src/main/kotlin/ch/learntrack/core/plugins/Serialization.kt
@@ -0,0 +1,16 @@
+package ch.learntrack.core.plugins
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.serialization.jackson.jackson
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) { jackson { enable(SerializationFeature.INDENT_OUTPUT) } }
+    routing { get("/json/jackson") { call.respond(mapOf("hello" to "world")) } }
+}
